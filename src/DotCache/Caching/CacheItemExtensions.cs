cb861385c03087bed09a04220adaf1cb60a9faab
Carlton Upperdine <carlton.upperdine@gmail.com>
2023-01-10 15:23:16 +0000

    move extensions from abstractions project- not required here

--- src/DotCache/Caching/CacheItemExtensions.cs
+++ src/DotCache/Caching/CacheItemExtensions.cs
@@ -1,10 +1,13 @@
-namespace DotCache.Abstractions.Caching;
+using DotCache.Abstractions.Caching;
+
+namespace DotCache.Caching;
 
 public static class CacheItemExtensions
 {
     public static bool IsExpired(this CacheItem cacheItem)
     {
         if (cacheItem.ExpiryDate is null) return false;
+        if (cacheItem.ExpiryDate == DateTime.MinValue) return false;
         return cacheItem.ExpiryDate <= DateTime.Now;
     }
 }
